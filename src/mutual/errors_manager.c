337669b9aaacd68556cff690cb10bc0ca61cf1bf
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-05-07 16:15:47 +0200

    errors display in stderr

--- src/mutual/errors_manager.c
+++ src/mutual/errors_manager.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/28 17:35:03 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/04/24 11:17:47 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/05/07 16:15:26 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ void	ft_error(t_heaps **ab, char *precision)
 {
 	if (precision && ft_strcmp(precision, "NO") != 0)
 	{
-		ft_putstr(precision);
-		ft_putstr("() ");
-		ft_putstr("Error\n");
+		ft_putstr_fd(precision, 2);
+		ft_putstr_fd("() ", 2);
+		ft_putstr_fd("Error\n", 2);
 	}
 	else
-		ft_putstr("Error\n");
+		ft_putstr_fd("Error\n", 2);
 	if ((*ab))
 		ft_heaps_del(ab);
 	exit(0);
@@ -31,12 +31,12 @@ void	ft_error_sdl(t_heaps **ab, char *precision, char *precision2)
 {
 	if (precision && ft_strcmp(precision, "NO") != 0)
 	{
-		ft_printf("%s() Error\n", precision);
+		ft_dprintf(2, "%s() Error\n", precision);
 		if (precision2)
-			ft_printf("%s\n", precision2);
+			ft_dprintf(2, "%s\n", precision2);
 	}
 	else
-		ft_putstr("Error\n");
+		ft_putstr_fd("Error\n", 2);
 	if ((*ab))
 		ft_heaps_del(ab);
 	exit(0);
